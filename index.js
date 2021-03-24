142767b4b12cd851106d3dbcfbaed6eca1df7d6f
Charlene Johnson <charcodes24@gmail.com>
2021-03-24 15:12:18 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    function clickAlert() {
+        alert('I was clicked!')
+    };
+    input.addEventListener('click', clickAlert);
+};
+
+
+/*function clickAlert() {
+    alert('I was clicked!');
+  }
+
+const five = document.querySelector('div:nth-child(5)')
+
+function addingEventListener(element) {
+    element.addEventListener('click', clickAlert);
 }
+
+addingEventListener(five);*/
